dd3e1842bb3e77afbcc1585d0a04192d07117fe1
문경진 <mkj0331@naver.com>
2025-07-25 12:05:14 +0900

    problem_a_제출1

--- problem/problem_a.py
+++ problem/problem_a.py
@@ -1,10 +1,43 @@
 # TMDB API 키 설정
-API_KEY = '561295c5763e591775f72a3dfd5e9400'
+from pprint import pprint
+
+API_KEY = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1NjEyOTVjNTc2M2U1OTE3NzVmNzJhM2RmZDVlOTQwMCIsIm5iZiI6MTc1MzQxMDA1MC43MDIwMDAxLCJzdWIiOiI2ODgyZWEwMjI2ODM0Mzk1Njg1NWUyZGYiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.p8dXSMvDJxL3o7HTjoNYaGx0vIUzvdv5HuB7MTMtc-c'
 BASE_URL = ''
+
+import requests, csv
+
+#account_id = youngseok99
+url = "https://api.themoviedb.org/3/movie/popular"
+
+headers = {
+    "accept": "application/json",
+    "Authorization": "bearer " + API_KEY
+}
+
+response = requests.get(url, headers=headers).json()
+response = response['results']
+
+
 
 # API 호출 함수
 
 # 영화 데이터 처리 함수
 
 # 데이터 수집 및 CSV 파일로 저장
+
+# 인기 영화 목록에서 id, title, release_date, popularity를 추출하여 CSV로 저장
+
+problem_a = []
+fields = ['id', 'title', 'release_date', 'popularity']
+for item in response:
+    temp_item = {}
+    for key in fields:
+        temp_item[key] = item[key]
+    problem_a.append(temp_item)
+
+with open('completed_problem_a.csv', 'w', newline='', encoding='utf-8') as file:
+    writer = csv.DictWriter(file, fields)
+    writer.writeheader()
+    writer.writerows(problem_a)
+
 
