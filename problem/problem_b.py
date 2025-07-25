233cde3f1b38f9fd9f7df83b5bf4c9f1b0a17198
Youngseok Song <youngseok99@naver.com>
2025-07-25 12:08:11 +0900

    middle save 1

--- problem/problem_b.py
+++ problem/problem_b.py
@@ -1,26 +1,32 @@
 # TMDB API 키 설정
-from pprint import pprint
+import requests
+import csv
+from tqdm import tqdm
+
+# API 호출 함수
 
 API_KEY = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1NjEyOTVjNTc2M2U1OTE3NzVmNzJhM2RmZDVlOTQwMCIsIm5iZiI6MTc1MzQxMDA1MC43MDIwMDAxLCJzdWIiOiI2ODgyZWEwMjI2ODM0Mzk1Njg1NWUyZGYiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.p8dXSMvDJxL3o7HTjoNYaGx0vIUzvdv5HuB7MTMtc-c'
 BASE_URL = ''
-
-import requests
-
-#account_id = youngseok99
-url = "https://api.themoviedb.org/3/movie/popular"
 
 headers = {
     "accept": "application/json",
     "Authorization": "bearer " + API_KEY
 }
 
-response = requests.get(url, headers=headers).json()
-
-pprint(response)
-
-# API 호출 함수
-
 # 영화 상세 데이터 처리 함수
+
+popular_movies = requests.get("https://api.themoviedb.org/3/movie/popular", headers=headers).json()
+id_list = []
+for item in tqdm(popular_movies['results']):
+    id_list.append(item['id'])
+
+with open('details.csv', 'w') as file:
+    fieldnames = ['budget','revenue', 'runtime', 'genres' ]
+    csv_writer = csv.DictWriter(file, fieldnames=fieldnames)
+    #for id in tqdm(id_list):
+    #temp_dict = {}
+    details = requests.get("https://api.themoviedb.org/3/tv/968171", headers=headers).json()
+    print(details)
 
 # CSV 파일에서 영화 ID 읽기
 
