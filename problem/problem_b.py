6040e11cb23e0e7bc8eb9504f99a0f5861f41a9e
Youngseok Song <youngseok99@naver.com>
2025-07-25 11:39:34 +0900

    found out routes

--- problem/problem_b.py
+++ problem/problem_b.py
@@ -1,19 +1,22 @@
 # TMDB API 키 설정
+from pprint import pprint
+
 API_KEY = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1NjEyOTVjNTc2M2U1OTE3NzVmNzJhM2RmZDVlOTQwMCIsIm5iZiI6MTc1MzQxMDA1MC43MDIwMDAxLCJzdWIiOiI2ODgyZWEwMjI2ODM0Mzk1Njg1NWUyZGYiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.p8dXSMvDJxL3o7HTjoNYaGx0vIUzvdv5HuB7MTMtc-c'
 BASE_URL = ''
 
 import requests
 
-url = "https://api.themoviedb.org/3/authentication"
+#account_id = youngseok99
+url = "https://api.themoviedb.org/3/movie/popular"
 
 headers = {
     "accept": "application/json",
     "Authorization": "bearer " + API_KEY
 }
 
-response = requests.get(url, headers=headers)
+response = requests.get(url, headers=headers).json()
 
-print(response.text)
+pprint(response)
 
 # API 호출 함수
 
