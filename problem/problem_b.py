6ad07eeb5a2dfc0bdb94bf8107903a1c77cfde95
Youngseok Song <youngseok99@naver.com>
2025-07-25 11:31:42 +0900

    changed api key

--- problem/problem_b.py
+++ problem/problem_b.py
@@ -1,6 +1,19 @@
 # TMDB API 키 설정
-API_KEY = 'YOUR_API_KEY'
+API_KEY = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1NjEyOTVjNTc2M2U1OTE3NzVmNzJhM2RmZDVlOTQwMCIsIm5iZiI6MTc1MzQxMDA1MC43MDIwMDAxLCJzdWIiOiI2ODgyZWEwMjI2ODM0Mzk1Njg1NWUyZGYiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.p8dXSMvDJxL3o7HTjoNYaGx0vIUzvdv5HuB7MTMtc-c'
 BASE_URL = ''
+
+import requests
+
+url = "https://api.themoviedb.org/3/authentication"
+
+headers = {
+    "accept": "application/json",
+    "Authorization": "bearer " + API_KEY
+}
+
+response = requests.get(url, headers=headers)
+
+print(response.text)
 
 # API 호출 함수
 
