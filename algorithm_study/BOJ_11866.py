af0dfafb8e67e605f7ac301dc3c3978e7c55d514
문경진 <mkj0331@naver.com>
2025-08-05 22:33:18 +0900

    boj_11866

--- algorithm_study/BOJ_11866.py
+++ algorithm_study/BOJ_11866.py
@@ -1,12 +1,24 @@
-N, K = 7, 3
+# N, K = 7, 3
+
+# people = list(range(1, N+1))
+# output = []
+# idx = 0 
+# # idx = 2 -> 4 -> 1 -> 3 -> 2 -> 0 -> 0
+# while people:
+#     idx = (idx + K - 1) % len(people)
+#     output.append(people.pop(idx))
+
+# res = ", ".join(map(str, output))
+# print(f"<{res}>")
+
+N, K = map(int, input().split())
 
 people = list(range(1, N+1))
 output = []
-idx = 2
+idx = 0
 while people:
+    idx = (idx + K - 1) % len(people)
     output.append(people.pop(idx))
-    idx += 3
-print(output)
-print(people)
 
-# 8 -> 2
+res =  ", ".join(map(str, output))
+print(f"<{res}>")
