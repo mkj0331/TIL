f50e61bea2734fc8178adb92d164895063e4f4fc
문경진 <mkj0331@naver.com>
2025-08-29 13:25:53 +0900

    25.08.29

--- TIL/06_algorithm_adv/03-shortestpath/100_offline/5251_최소이동거리/my_sol.py
+++ TIL/06_algorithm_adv/03-shortestpath/100_offline/5251_최소이동거리/my_sol.py
@@ -3,34 +3,36 @@ sys.stdin = open('input.txt')
 
 import heapq
 
+
 def distra():
     distances = [float('inf')] * (N+1)
+    distances[0] = 0
     visited = set()
     heap = []
-    # 누적 거리, 시작 정점
-    heapq.heappush(heap, (0, 0))
-    distances[0] = 0
+    heapq.heappush(heap, (0, 0)) # 시작 거리, 시작 정점
 
     while heap:
-        weight, current = heapq.heappop(heap)
-        if current in visited: continue
-        visited.add(current)
+        current_distance, current_vertex = heapq.heappop(heap)
+        if current_vertex in visited:
+            continue
+        visited.add(current_vertex)
 
-        for next, next_weight in graph[current].items():
-            acc = weight + next_weight
-            if acc < distances[next]:
-                distances[next] = acc
-                heapq.heappush(heap, (acc, next))
+        for next_vertex, next_distance in graph[current_vertex].items():
+            acc = current_distance + next_distance
+            if acc < distances[next_vertex]:
+                distances[next_vertex] = acc
+                heapq.heappush(heap, (acc, next_vertex))
     return distances
+
 
 T = int(input())
 for t in range(1, T+1):
     N, E = map(int, input().split())
-    data = [list(map(int, input().split())) for _ in range(E)]
-
     graph = {v: {} for v in range(N+1)}
-    for s, e, w in data:
+    for _ in range(E):
+        s, e, w = map(int, input().split())
         graph[s][e] = w
 
     res = distra()
-    print(f'#{t} {res[N]}')
+    print(res[N])
+
